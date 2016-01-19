15e04ce21a5f48d44f433fd5f71934ff897f182d
Ekushisu <thomas.labeyrie@outlook.com>
2016-01-19 13:38:02 +0100

    Add changing turn system

--- js/init.js
+++ js/init.js
@@ -1,26 +1,14 @@
-(function($){
-  $(function(){
-
+var game;
+$(document).ready(function(){
     $('.button-collapse').sideNav();
     $('#modal1').openModal();
     $('#modal1 form').submit(function(e){
-    	e.preventDefault();
-    	e.stopPropagation();
-    	var game = new Game($('input[name=player1name]').val(),$('input[name=player2name]').val());
-    	game.init();
-
-        setInterval(function(e){
-            console.log(game.getSelectedSquare());
-        },200)
+        e.preventDefault();
+        e.stopPropagation();
+        game = new Game($('input[name=player1name]').val(),$('input[name=player2name]').val());
+        game.init();
     })
-
-    $('#restartGameButton').click(function(){
-    	location.reload();
-    })
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
-
+});
 
 var Game = function(player1name, player2name){
 	this.player1 = {
@@ -43,36 +31,44 @@ Game.prototype.init = function() {
     $('#player2nameholder').html(this.player2.name);
     $('#currentplayernameholder').html(this.player1.name);
     $('.square[data-belong=false]').click(function(){
-        self.OnSelectSquare($(this));
+        self.onSelectSquare($(this));
+    });
+    $('#validateTourButton').click(function(){
+        self.onValidateTour();
+    });
+    $('#restartGameButton').click(function(){
+        location.reload();
     });
     $('#modal1').closeModal();
 };
 
 
-// Setter for selectSquare;
+// Setter for selectedSquare;
 Game.prototype.setSelectedSquare = function($elem) {
     $elem = $elem || null
     if ($elem)
-        this.selectSquare = this.getSquareName($elem);
+        //this.selectedSquare = this.getSquareName($elem);
+        this.selectedSquare = $elem;
     else
-        this.selectSquare = null;
+        this.selectedSquare = null;
 
-    return this.selectSquare;
+    return this.selectedSquare;
 };
 
-// Getter for selectSquare;
+// Getter for selectedSquare;
 Game.prototype.getSelectedSquare = function() {
-    return this.selectSquare;
+    return this.selectedSquare;
 };
 
 // Return the name of a clicked element to use it as a slug (sq-x-y)
-Game.prototype.getSquareName = function($elem) {
+// Deprecated
+/*Game.prototype.getSquareName = function($elem) {
     return "sq-" + $elem.attr('data-x') + "-" + $elem.attr('data-y');
-};
+};*/
 
 
 Game.prototype.setCurrentTurn = function(player) {
-    this.gameTurn = player.slug;
+    this.gameTurn = player;
     console.log('The hand is to ' + player.name + '(' + player.slug + ')');
     return this.gameTurn;
 };
@@ -80,21 +76,38 @@ Game.prototype.setCurrentTurn = function(player) {
 // Return the slug of current player term
 Game.prototype.getCurrentTurn = function()
 {
-    return this.gameTurn.slug;
+    return this.gameTurn;
 }
 
-// Triggered when a square is clicked
-Game.prototype.OnSelectSquare = function($elem) {
+Game.prototype.changeTurn = function() {
+    console.log(this.getCurrentTurn().slug);
+    if (this.getCurrentTurn().slug == "player1")
+    {
+        this.setCurrentTurn(this.player2);
+        $('body').attr("data-currentTurn", this.getCurrentTurn().slug);
+    } else {
+        this.setCurrentTurn(this.player1);
+        $('body').attr("data-currentTurn", this.getCurrentTurn().slug);
+        $('#currentplayernameholder').html(this.getCurrentTurn().name);
+    }
 
-    if ($elem.attr('data-belong') != "false")
+    return true;
+};
+
+// Triggered when a square is clicked
+Game.prototype.onSelectSquare = function($elem) {
+
+    if ($elem.attr('data-belong') != "false") {
+        Materialize.toast("La case est déjà marqué par " + this[$elem.attr('data-belong')].name, 5000);
         return false;
+    }
 
     if ($elem.attr('data-selected') == "false")
     {
         // Changing the selectedSquare
         this.setSelectedSquare($elem);
         $('.square:not([data-selected=false])').attr('data-selected', "false");
-        $elem.attr('data-selected',this.getCurrentTurn());
+        $elem.attr('data-selected',this.getCurrentTurn().slug);
         $('#validateTourButton').removeClass('disabled');
     } else {
         // Unselect
@@ -102,6 +115,14 @@ Game.prototype.OnSelectSquare = function($elem) {
         $('.square:not([data-selected=false])').attr('data-selected', "false");
         $('#validateTourButton').addClass('disabled');
     }
-    
+
     return true;
+};
+
+Game.prototype.onValidateTour = function() {
+    console.log("Validation du tour");
+    $('#validateTourButton').addClass('disabled');
+    this.getSelectedSquare().attr('data-selected', false);
+    this.getSelectedSquare().attr('data-belong', this.getCurrentTurn().slug);
+    this.changeTurn();
 };
