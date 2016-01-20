22251870a05aa9090935881d1e42a865acfb1480
Ekushisu <thomas.labeyrie@outlook.com>
2016-01-20 10:47:40 +0100

    add endgame logic

--- js/init.js
+++ js/init.js
@@ -1,5 +1,56 @@
 
 var game;
+var gameMap = {
+    1 : {
+        coord : [0,0],
+        hoodies : [2,5,4]
+    },
+    2 : {
+        coord : [0,1],
+        hoodies : [1,4,5,6,3]
+    },
+    3 : {
+        coord : [0,2],
+        hoodies : [2,5,6]
+    },
+    4 : {
+        coord : [1,0],
+        hoodies : [1,2,5,8,7]
+    },
+    5 : {
+        coord : [1,1],
+        hoodies : [1,2,3,4,6,7,8,9]
+    },
+    6 : {
+        coord : [1,2],
+        hoodies : [3,2,5,8,9]
+    },
+    7 : {
+        coord : [2,0],
+        hoodies : [4,5,8]
+    },
+    8 : {
+        coord : [2,1],
+        hoodies : [7,4,5,6,9]
+    },
+    9 : {
+        coord : [2,2],
+        hoodies : [6,5,8]
+    }
+};
+
+var gamePiano = {
+    1 : null,
+    2 : null,
+    3 : null,
+    4 : null,
+    5 : null,
+    6 : null,
+    7 : null,
+    8 : null,
+    9 : null
+};
+
 jQuery.fn.exists = function(){return this.length>0;}
 
 $(document).ready(function(){
@@ -22,11 +73,13 @@ $(document).ready(function(){
 var Game = function(player1name, player2name){
 	this.player1 = {
         slug : 'player1',
-		name : player1name
+		name : player1name,
+        turnNumber : 0
 	};
 	this.player2 = {
         slug : 'player2',
-		name : player2name
+		name : player2name,
+        turnNumber : 0
 	};
     this.selectedSquare = false;
 	this.gameTurn = this.player1;
@@ -70,12 +123,14 @@ Game.prototype.getSelectedSquare = function() {
     return this.selectedSquare;
 };
 
-// Return the name of a clicked element to use it as a slug (sq-x-y)
-// Deprecated
-/*Game.prototype.getSquareName = function($elem) {
-    return "sq-" + $elem.attr('data-x') + "-" + $elem.attr('data-y');
-};*/
+Game.prototype.increasePlayerTurnNumber = function() {
+    this.getCurrentTurn().turnNumber++;
+    return true;
+};
 
+Game.prototype.getCurrentPlayerTurnNumber = function() {
+    return this.getCurrentTurn().turnNumber;
+};
 
 Game.prototype.setCurrentTurn = function(player) {
     this.gameTurn = player;
@@ -104,55 +159,70 @@ Game.prototype.changeTurn = function() {
 
 Game.prototype.findEndGame = function() {
     var self = this;
-    var freeCaseCount = $('[data-belong=false]').length;
 
-    // Game map full
-    if (freeCaseCount == 0) {
-        $('#endHeadText').html('Aucun gagnant sur cette partie :/')
-        $('#endGameReason').html('La grille a été complété');
-        $('#modalEndGame').openModal({
-          dismissible: false, // Modal can be dismissed by clicking outside of the modal
-          opacity: .2, // Opacity of modal background
-          in_duration: 300, // Transition in duration
-          out_duration: 200, // Transition out duration
-        });
-    };
+    console.log("Check fin du jeu");
+
+    if (this.getCurrentPlayerTurnNumber() < 3)
+        return false;
+
+    console.log("On a joué plus de 2 fois");
 
     // Check if player 1 got a line
     var lineFound = false; // initialization
 
-    $('.square').each(function(){
-       /* var aligned = 0;
-        var currentX = $(this).data('x');
-        var currentY = $(this).data('y');
+    if (gamePiano[1] != null && gamePiano[1] == gamePiano[4] && gamePiano[1] == gamePiano[7])  
+        lineFound = [1,4,7];
+    if (gamePiano[2] != null && gamePiano[2] == gamePiano[5] && gamePiano[2] == gamePiano[8])  
+        lineFound = [2,5,8];
+    if (gamePiano[3] != null && gamePiano[3] == gamePiano[6] && gamePiano[3] == gamePiano[9])  
+        lineFound = [3,6,9];
+    if (gamePiano[1] != null && gamePiano[1] == gamePiano[2] && gamePiano[1] == gamePiano[3])  
+        lineFound = [1,2,3];
+    if (gamePiano[5] != null && gamePiano[5] == gamePiano[6] && gamePiano[5] == gamePiano[4])  
+        lineFound = [5,6,4];
+    if (gamePiano[8] != null && gamePiano[8] == gamePiano[9] && gamePiano[8] == gamePiano[7])  
+        lineFound = [8,9,7];
+    if (gamePiano[1] != null && gamePiano[1] == gamePiano[5] && gamePiano[1] == gamePiano[9])  
+        lineFound = [1,5,9];
+    if (gamePiano[3] != null && gamePiano[3] == gamePiano[5] && gamePiano[3] == gamePiano[7])  
+        lineFound = [3,5,7];
+    if (gamePiano[3] != null && gamePiano[3] == gamePiano[5] && gamePiano[3] == gamePiano[7])  
+        lineFound = [3,5,7];
 
-        for (var x = (currentX - 1); x <= (currentX + 1; x++) {
-            for (var y = (currentY - 1); y <= (currentX + 1); y++) {
-                var testedCase = $(".square[data-x=" + x + "][data-y=" + y + "]");
-                if (!testedCase.exists())
-                    continue;
 
-                if (testedCase.data('belong') != self.player1.slug)
-                    continue;
+    console.log(lineFound);
 
-                if (currentX == x && currentY == y)
-                    continue;
-                aligned++;
-            };
+
+    if (!lineFound) {
+        console.log("Pas de ligne trouvé");
+        var freeCaseCount = $('[data-belong=false]').length;
+        if (freeCaseCount == 0) {
+            $('#endHeadText').html('Aucun gagnant sur cette partie :/')
+            $('#endGameReason').html('La grille a été complétée');
+            $('#modalEndGame').openModal({
+              dismissible: false, // Modal can be dismissed by clicking outside of the modal
+              opacity: .2, // Opacity of modal background
+              in_duration: 300, // Transition in duration
+              out_duration: 200, // Transition out duration
+            });
+
         };
+        return false;
+    }
 
-        if (aligned >= 2) {
-            alert("WE GOT A WINNEr");
-        };*/
+    var winner = $('.square[data-id=' + lineFound[0] + ']').data('belong');
+    console.log(winner);
+    
+    $('#endHeadText').html(this[winner].name + ' a gagné la partie !');
+    $('#endGameReason').html('Une ligne a été complétée');
+    $('#modalEndGame').openModal({
+      dismissible: false, // Modal can be dismissed by clicking outside of the modal
+      opacity: .2, // Opacity of modal background
+      in_duration: 300, // Transition in duration
+      out_duration: 200, // Transition out duration
     });
 
-    /*for (var x = 0; x <= 2; x++) {
-        for (var y = 0; y <= 2; y++) {
-            var testedCase = $(".square[data-x=" + x + "][data-y=" + y + "]");
-            if (testedCase.data('belong') != self.player1.slug)
-                continue;
-        };
-    };*/
+    return true;
 };
 
 // Triggered when a square is clicked
@@ -190,6 +260,9 @@ Game.prototype.onValidateTour = function() {
     $('#validateTourButton').addClass('disabled');
     this.getSelectedSquare().attr('data-selected', false);
     this.getSelectedSquare().attr('data-belong', this.getCurrentTurn().slug);
+    gamePiano[this.getSelectedSquare().data('id')] = this.getCurrentTurn().slug;
+    console.log(gamePiano);
+    this.increasePlayerTurnNumber();
     this.findEndGame();
     this.changeTurn();
 
