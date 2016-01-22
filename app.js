0bbc835fa6d9844c19c9e9d9cbb71f994fed8475
Flamm <tom0016@hotmail.fr>
2016-01-22 22:19:08 +0100

    Finished socketisation of existing code

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 
-var express = require('express');
-var app = express();
-var twig = require('twig');
-var http = require('http').Server(app);
-var routes = require(__dirname + '/app/routes.json');
-var globalData = {
-    "routes" : routes
+var express = require('express'),
+    app = express(),
+    twig = require('twig'),
+    http = require('http').Server(app),
+    routes = require(__dirname + '/app/routes.json'),
+    ON_DEATH = require('death'),
+    prompt = require('prompt'),
+    utils = require(__dirname + '/app/utils.js');
+
+try {
+    var config = require(__dirname + '/app/config.json');
+} catch (e) {
+    if (e.code == "MODULE_NOT_FOUND")
+        console.log('No config file found in /app/, please use config.exemple.json and rename it in config.json');
+
+    console.log(e);
+    process.exit();
 }
 
-
-var injectGlobalData = function(o) {
-    for (var key in globalData) {
-        o[key] = globalData[key];
-    }
-    return o;
+var globalData = {
+    "routes" : routes,
+    "config" : config
 }
 
 app.set('views', __dirname + '/views');
@@ -22,23 +29,39 @@ app.engine('html', twig.__express);
 app.use(express.static(__dirname + '/public/assets'));
 
 app.get(routes.home, function(req, res){
-    res.render('index.twig', new injectGlobalData({
-        "test" : "test"
-    }));
+    res.render('index.twig', new utils.injectGlobalData(globalData,{}));
 });
 
 app.get(routes.game, function(req, res){
-    res.render('game.twig');
+    res.render('game.twig', new utils.injectGlobalData(globalData,{}));
 });
 
 app.get(routes.gameAI, function(req, res){
-    res.render('gameAI.twig');
+    res.render('gameAI.twig', new utils.injectGlobalData(globalData,{}));
 });
 
 app.get(routes.gameOnline, function(req, res){
-    res.render('gameOnline.twig');
+    res.render('gameOnline.twig', new utils.injectGlobalData(globalData,{}));
 });
 
 http.listen(3000, function(){
-    console.log('listening on *:3000');
+    console.log("\n \n  ################################################");
+    console.log("  ## Hello, I'm running fast for you :) ♥ nodeJS ");
+    console.log("  ## Server is now running on :  " + config.host + ":" + config.port);
+    console.log("  ################################################ \n \n");
+});
+
+ON_DEATH(function(signal, err) {
+    new utils.promptExit(prompt,config);
+})
+
+process.on('uncaughtException', function(err) {
+    if(err.errno === 'EADDRINUSE') {
+        console.log("  ######################################################");
+        console.log("  ## Hmm, something is already running on " + config.host + ":" + config.port);
+        console.log("  ###################################################### \n \n");
+    } else {
+        console.log(err);
+    }
+    process.exit(1);
 });
